2567257cb143f14c2cf57ba6f8624fe8fd3b93c7
nrmaanrfnd7 <161284310+nrmaanrfnd7@users.noreply.github.com>
2025-01-08 11:37:26 +0700

    Delete CRUD_MVC_NOURMAN_ROFIANDI directory

--- CRUD_MVC_NOURMAN_ROFIANDI/src/main/java/com/pbo/crud_mvc/database/TestConection.java
+++
@@ -1,9 +0,0 @@
-package com.pbo.crud_mvc.database;
-
-import com.pbo.crud_mvc.database.DatabaseConection;
-
-public class TestConection {
-    public static void main(String[] args) {
-        DatabaseConection.connect();
-    }
-}
